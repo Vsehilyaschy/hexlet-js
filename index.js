38b85cb89f52afbd9f50c47a895a5a27a7da9576
Nikita <Mobinik@list.ru>
2024-06-28 16:47:03 +0400

    fix index.js and src/index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-import lastNumbers from "./src/index.js";
-const numbers = ["one", "two"];
+import lastNumbers from './src/index.js';
+
+const numbers = ['one', 'two'];
 console.log(lastNumbers(numbers));
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 export default function lastNubmers(array) {
-  var length = array == null ? 0 : array.length;
+  const length = array == null ? 0 : array.length;
   return length ? array[length - 1] : undefined;
 }
